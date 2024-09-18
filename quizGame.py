62b112a99bafd2bc5f7088aa60de2c199b7db706
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-18 16:34:41 +0100

    Updated questions in quizGame.py

--- quizGame.py
+++ quizGame.py
@@ -1,6 +1,11 @@
 # Python quiz game
 
-question = ()
+question = ("How many elements are in the periodic table?: ",
+            "Which animalays the largest eggs?: ",
+            "What is the most abundant gas in Eath's atmosphere?: ",
+            "How many bones are in the human body?: ",
+            "Which planet in the solar system is the hottest?: "
+            )
 
 options = ((), (), (), (), ())
 
