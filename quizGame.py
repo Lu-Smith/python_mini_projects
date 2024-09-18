b98df8254197b557451e7d9c75f8317f40f81e23
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-18 16:52:46 +0100

    Updated logic in quizGame.py

--- quizGame.py
+++ quizGame.py
@@ -13,7 +13,7 @@ options = (("A. 116", "B. 117", "C. 118", "D. 119"),
            ("A. 206", "B. 207", "C. 208", "D. 209"), 
            ("A. Mercury", "B. Venus", "C. Earth", "D. Mars"))
 
-anserws = ("C", "D", "A", "A", "B")
+answers = ("C", "D", "A", "A", "B")
 guesses = []
 score = 0
 question_num = 0
@@ -21,3 +21,16 @@ question_num = 0
 for question in questions:
     print("--------------")
     print(question)
+    for option in options[question_num]:
+        print(option)
+        
+    guess = input("Enter (A, B, C, D): ").upper()
+    guesses.append(guess)
+    if guess == answers[question_num]:
+        score += 1
+        print("Correct 😀")
+    else:
+        print("Incorrect 🥺")
+        print(f"{answers[question_num]} is the correct answer.")
+        
+    question_num += 1
